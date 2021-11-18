b5f89d8c97af1cb42a84fdde47c37e5d4bc51a55
Rootdrigo <errecastillo@gmail.com>
2021-11-18 15:40:12 -0500

    bug de mierda

--- timing_attacks_simple_comp.py
+++ timing_attacks_simple_comp.py
@@ -15,7 +15,7 @@ def comp(str1 , str2):
 
 
 def simple_login(password):
-    real_password = "bad"
+    real_password = "AOLA"
     if(comp(real_password , password)):
         print("You're in'")
     else:
@@ -47,6 +47,44 @@ def crack_password_lengh():
     #ahora agarro el mayor y ya
     password_lenght = np.argmax(promedios)
     print(f"la longitud de la llave es {password_lenght}")
+    return(password_lenght)
 
-crack_password_lengh()
+def replace_index(string,index , letter):
+    try:
+        string = list(string)
+        string[index] =  letter
+        string = "".join(string)
+        return(string)
+    except:
+        return(string)
+
+def crack_key():
+    dictionary = [chr(i) for i in range(ord("A"),ord("Z"))]
+    password_lenght = crack_password_lengh()
+    password = []
+    palabra_prueba = create_bad_password(password_lenght)
+    #entonces ahora lo que hacemos es intentar cada caracter n veces y según el tiempo de respuesta pues nos quedamos con el mayor
+    for i in range(password_lenght):
+        #intento todas las palabras del diccionario y calculo la longitud, me quedo con la mejor
+        promedios_total = []
+        for j in dictionary:
+            #intento 1000 veces cada palabra para agarrar el promedio
+            prom = []
+            for i in range(10000):
+                palabra_prueba = replace_index(palabra_prueba , i , j) #reemplazo la iesima letra por una letra en el diccionario
+                #intento entrar,  me quedo con el mejor
+                init = time.time()
+                simple_login(palabra_prueba)
+                end = time.time()
+                total_time = end-init
+                prom.append(total_time)
+            #meto el promedo
+            promedios_total.append(np.mean(prom))
+        index_mejor_letra = np.argmax(promedios_total)
+        mejor_letra = dictionary[index_mejor_letra]
+        #palabra_prueba = replace_index(palabra_prueba , i , mejor_letra)
+        print(palabra_prueba)
+
+#crack_password_lengh()
+crack_key()
 
